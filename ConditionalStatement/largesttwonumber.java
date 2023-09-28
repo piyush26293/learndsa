0f6fcb7643f13c0c00a852049a2c68b3d5a89df5
piyushahirwar <piyush.ahirwar@arietis.co.in>
2023-09-29 01:15:32 +0530

    Create largesttwonumber.java

---
+++ ConditionalStatement/largesttwonumber.java
@@ -0,0 +1,13 @@
+public class largesttwonumber {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 200;
+
+        if(a>=b){
+            System.out.println("A is lagest "+a);
+        }
+        else {
+            System.out.println("B is largest "+b);
+        }
+    }
+}
